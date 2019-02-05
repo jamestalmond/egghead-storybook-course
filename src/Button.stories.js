8c18d1ae9d15094158318c411fd3e9d2be25b963
James Almond <jamestalmond@gmail.com>
2019-02-05 13:51:24 +0000

    Added text knob to button renders

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -23,11 +23,17 @@ storiesOf('Buttons', module)
 			<Button>Click here </Button>
 			~~~
 			`
-		})(() => <Button bg={color('bg', '#7fba23')}>Add to basket</Button>)
+		})(() => (
+			<Button bg={color('bg', '#7fba23')}>
+				{text('text', 'Add to basket')}
+			</Button>
+		))
 	)
 	.addWithJSX('Secondary action', () => (
-		<Button bg={color('bg', '#0074cc')}>Add to basket</Button>
+		<Button bg={color('bg', '#0074cc')}>
+			{text('text', 'More information')}
+		</Button>
 	))
 	.addWithJSX('Disabled', () => (
-		<Button bg={color('bg', '#afb7c5')}>Add to basket</Button>
+		<Button bg={color('bg', '#afb7c5')}>{text('text', 'Out of stock')}</Button>
 	));
