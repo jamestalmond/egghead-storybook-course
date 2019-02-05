0785d4abdb7c84b5895a46bafcbe1d5e2f36fa35
James Almond <jamestalmond@gmail.com>
2019-02-05 13:47:29 +0000

    Storybook knobs

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -3,6 +3,7 @@ import { withInfo } from '@storybook/addon-info';
 
 import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
+import { text, color } from '@storybook/addon-knobs/react';
 
 storiesOf('Buttons', module)
 	.addWithJSX(
@@ -22,8 +23,11 @@ storiesOf('Buttons', module)
 			<Button>Click here </Button>
 			~~~
 			`
-		})(() => <Button bg="#7fba23">Add to basket</Button>)
+		})(() => <Button bg={color('bg', '#7fba23')}>Add to basket</Button>)
 	)
 	.addWithJSX('Secondary action', () => (
-		<Button bg="#0074cc">Add to basket</Button>
+		<Button bg={color('bg', '#0074cc')}>Add to basket</Button>
+	))
+	.addWithJSX('Disabled', () => (
+		<Button bg={color('bg', '#afb7c5')}>Add to basket</Button>
 	));
