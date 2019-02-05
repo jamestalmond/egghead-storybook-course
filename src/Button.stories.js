59ff7b23389b4cd2fd36efaf312cee3064fddba7
James Almond <jamestalmond@gmail.com>
2019-02-05 13:14:24 +0000

    Display the JSX of a React Storybook story with the JSX addon

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -4,5 +4,9 @@ import { storiesOf } from '@storybook/react';
 import { Button } from './Button';
 
 storiesOf('Buttons', module)
-	.add('Primary brand', () => <Button bg="#7fba23">Add to basket</Button>)
-	.add('Secondary action', () => <Button bg="#0074cc">Help me choose</Button>);
+	.addWithJSX('Primary brand', () => (
+		<Button bg="#7fba23">Add to basket</Button>
+	))
+	.addWithJSX('Secondary action', () => (
+		<Button bg="#0074cc">Help me choose</Button>
+	));
